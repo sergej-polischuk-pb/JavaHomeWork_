6b49ba5ec83277c35d80e0a4f7c748d96c3754c3
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-20 16:18:34 +0300

    init commit

---
+++ JavaHomeWork/src/com/pb/poliscuk/hw2/Calculator.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pb.poliscuk.hw2;
+import java.util.Scanner;
+import java.lang.String;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int operand1;
+        int operand2;
+        String sign;    // переменная хранит тип операции
+        char repeat;    // считываем в цикле "Y" or "N"
+
+        // В цикле крутим тело программы пока пользователь не ответит "No"
+        do {
+            System.out.print("Введите целое число. Operand1 = ");
+            operand1 = scan.nextInt();
+            System.out.print("Введите целое число. Operand2 = ");
+            operand2 = scan.nextInt();
+            System.out.print("Введите знак арифметической операции. Sing = ");
+            sign = scan.next();
+            // определим какой арифметический знак ввел пользователь
+            switch (sign) {
+                case "+":
+                    System.out.println("Полученный результат : " + operand1 + "+" + operand2 + "=" + (operand1 + operand2));
+                    break;
+                case "-":
+                    System.out.println("Полученный результат : "  + operand1 + "-" + operand2 + "="+ (operand1 - operand2));
+                    break;
+                case "*":
+                    System.out.println("Полученный результат : " + operand1 + "*" + operand2 + "=" + (operand1 * operand2));
+                    break;
+                case "/": {
+                    if (operand2 == 0) System.out.println("Извините, но на НОЛЬ делить нельзя");
+                    else  System.out.println("Полученный результат "  +   operand1 + "/" + operand2 + "="+((double) operand1)/operand2);
+                    break;
+                }
+            }
+            System.out.print("Продолжим?  Y/N ");
+            repeat=scan.next().charAt(0);
+            repeat=Character.toUpperCase(repeat);
+        }
+        while (repeat != 'N' );
+    }
+}
