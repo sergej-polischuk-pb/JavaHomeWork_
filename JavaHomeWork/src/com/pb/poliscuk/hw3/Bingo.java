d02c4480190640577f9cdb2d2e09a0d0ab28452e
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-20 20:34:44 +0300

    удалил данные о лицензии. Добавил в текст автора

--- JavaHomeWork/src/com/pb/poliscuk/hw3/Bingo.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw3/Bingo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pb.poliscuk.hw3;
 import java.util.Scanner;
 import java.io.*;
@@ -10,7 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
- * @author serg
+ * @author Poliscuk Sergej
  */
 public class Bingo {
     public static void main(String[] args) {
