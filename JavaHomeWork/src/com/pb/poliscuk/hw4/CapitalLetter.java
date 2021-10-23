bb2c094bda48ca24536ba2849439f7387f7a693a
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-23 22:14:55 +0300

    init commit

--- JavaHomeWork/src/com/pb/poliscuk/hw4/CapitalLetter.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw4/CapitalLetter.java
@@ -9,26 +9,14 @@
 package com.pb.poliscuk.hw4;
 
 public class CapitalLetter {
-    public static void main (String[] args) {
-        StringBuilder string_example = new StringBuilder ("Ехали медведи на велосипеде.\n" +
-                                                          "А за ними кот задом наперёд.\n" +
-                                                          "А за ним комарики на воздушном шарике.\n" +
-                                                          "А за ними раки на хромой собаке.\n" +
-                                                          "Волки на кобыле. Львы в автомобиле.\n" +
-                                                          "Зайчики в трамвайчике. Жаба на метле… \n" +
-                                                          "Едут и смеются, пряники жуют..");
-        
-        // Выведем на экран первоначальную строку, а затем займемся ее разбором.....
-        System.out.println(string_example);
-        
-/*      Слова в языке разделяются симоволом "пробел"..
-        Начинаем посимвольно анализировать строку.
-*/
-        int i;  //счетчик для цикла
+    
+   public static  String  Letter_UpperCase_String(StringBuilder string_example)
+   {
+         int i;  //счетчик для цикла
                
         // Движемся в цикле по сторке в поисках символа "Пробел"
          for(i=0; i<=string_example.length()-1; i++)
-          {
+           {
               // Если я обнаруживаю в строке символ "пробел"
               // следующему символу необходимо сделать UpperCase
              
@@ -36,10 +24,33 @@ public class CapitalLetter {
               {
                 string_example.setCharAt(i+1,Character.toUpperCase(string_example.charAt(i+1)));
               }
-              
-          }
-         // Выводим строку после преобразования символов в врехний регистр
-         System.out.println("***************************************************************");
-         System.out.println(string_example);
+            }
+        //System.out.println(string_example);
+        //return string_example.toString();
+        return string_example.toString();
+   
+   }
+   
+
+   
+    
+    public static void main (String[] args) {
+        StringBuilder string_ = new StringBuilder ("Ехали медведи на велосипеде.\n" +
+                                                          "А за ними кот задом наперёд.\n" +
+                                                          "А за ним комарики на воздушном шарике.\n" +
+                                                          "А за ними раки на хромой собаке.\n" +
+                                                          "Волки на кобыле. Львы в автомобиле.\n" +
+                                                          "Зайчики в трамвайчике. Жаба на метле… \n" +
+                                                          "Едут и смеются, пряники жуют..");
+        
+            // Печатаем первоначальную строку
+              System.out.println("************ До  преобразования ***************************");
+            System.out.println(string_);
+            System.out.println("************ После  преобразования **************************");
+            // Вызываем нашу функцию.. 
+            System.out.println(Letter_UpperCase_String(string_));
+    
+
+        
 }
 }
