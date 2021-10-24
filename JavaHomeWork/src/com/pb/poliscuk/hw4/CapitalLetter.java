22d5f5014b6b90aab28728549c6a48662bc3f7dc
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-24 09:21:50 +0300

    init commit

--- JavaHomeWork/src/com/pb/poliscuk/hw4/CapitalLetter.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw4/CapitalLetter.java
@@ -7,9 +7,10 @@
 При выполнении задания использовать метод(ы).
  */
 package com.pb.poliscuk.hw4;
+import java.util.Scanner;
 
 public class CapitalLetter {
-    
+// Этот метод работает с переменной типа StringBuilder, разберу строку посимвольно...    
    public static  String  Letter_UpperCase_String(StringBuilder string_example)
    {
          int i;  //счетчик для цикла
@@ -25,7 +26,22 @@ public class CapitalLetter {
             }
         return string_example.toString();
    }
-    
+ // перегрузим метод под тип STRING
+public static  String  Letter_UpperCase_String(String string_example)
+   {
+         String upper_case_line = "";
+         Scanner lineScan = new Scanner(string_example);
+         //разобьем полученную строку на слова
+         while(lineScan.hasNext()) {
+         String word = lineScan.next(); 
+         // в каждом слове первый символ преобразовываем в верхний регистр
+         upper_case_line += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " "; 
+         }
+               
+       return upper_case_line;
+   }
+
+//****************** 
     
     public static void main (String[] args) {
         StringBuilder string_ = new StringBuilder ("Ехали медведи на велосипеде.\n" +
@@ -35,14 +51,25 @@ public class CapitalLetter {
                                                           "Волки на кобыле. Львы в автомобиле.\n" +
                                                           "Зайчики в трамвайчике. Жаба на метле… \n" +
                                                           "Едут и смеются, пряники жуют..");
+       
+        
+        String string2="Добрый доктор Айболит! Он под деревом сидит. Приходи к нему лечиться и корова, и волчица, и жучок, и червячок, и медведица!";
+        
         
             // Печатаем первоначальную строку
               System.out.println("************ До  преобразования ***************************");
             System.out.println(string_);
-            System.out.println("************ После  преобразования **************************");
+            System.out.println("************ После  преобразования тип - StringBuilder *******************");
             // Вызываем нашу функцию.. 
             System.out.println(Letter_UpperCase_String(string_));
-    
+            System.out.println("************ До  преобразования ***************************");
+            System.out.println(string2);
+            System.out.println("\n ************ После  преобразования тип - String *******************");
+            System.out.println(Letter_UpperCase_String(string2));
+            System.out.println("\n--===  А теперь введите свое предложение, которое я обработаю..  ===--");
+            Scanner scanner = new Scanner(System.in);
+            String user_string = scanner.nextLine();
+            System.out.println(Letter_UpperCase_String(user_string));
 
         
 }
