49a5c2c003fbc430cb5b0951ecdd4e217f09c22b
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-31 20:10:18 +0200

    init commit

--- JavaHomeWork/src/com/pb/poliscuk/hw5/Book.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw5/Book.java
@@ -9,26 +9,22 @@ package com.pb.poliscuk.hw5;
  *
  * @author serg
  */
-public  class Book {
+abstract class Book {
 
-   public String nameBook; // название книги
-   public String authorBook; //автор книги
-   public int yearEditionBook;  // год издания книги
-    
-    public void setBook (String nameBook, String authorBook, int yearEditionBook)
-    {
-       this.nameBook=nameBook;
-       this.authorBook=authorBook;
-       this.yearEditionBook=yearEditionBook;
+    public String nameBook; // название книги
+    public String authorBook; //автор книги
+    public int yearEditionBook;  // год издания книги
+
+    public void setBook(String nameBook, String authorBook, int yearEditionBook) {
+        this.nameBook = nameBook;
+        this.authorBook = authorBook;
+        this.yearEditionBook = yearEditionBook;
     }
-    
-    public void getBook()
-    {
+
+    public void getBook() {
         System.out.print(this.nameBook);
         System.out.print(this.authorBook);
         System.out.println(this.yearEditionBook);
     }
-    
-    
 
 }
--- JavaHomeWork/src/com/pb/poliscuk/hw5/Library.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw5/Library.java
@@ -2,29 +2,13 @@
  */
 package com.pb.poliscuk.hw5;
 
-/**
- *
- * @author serg
- */
-import com.pb.poliscuk.hw5.*;
-
-
 public class Library {
-    
-static Book[] bookLib = new Book[5];
+
+    static Book[] bookLib = new Book[5];
 
     public static void main(String[] args) {
-        
-         bookLib[0].setBook("Java для чайников", "Чак Сфер", 2006);
-        //bookLibrary[1].setBook("Полный справочник по С#", "Герберт Шилдт", 2005);
-        //bookLibrary[2].setBook("IBM PC для пользователя. Изд. 7-е, перераб. и доп.", "Фигурнов В.Э.", 1997);
-        //bookLibrary[3].setBook("Java 8. Руководство для начинающих", "Герберт Шилдт", 2015);
-        //bookLibrary[4].setBook("Самоучитель по созданию Web-страниц", "Гаевский А.Ю.", 2002);
-        
-        //**************** Выводим данные о книгах ***************************
-    
-        
-       // bookLib[0].getBook();
- 
+          bookLib[0].setBook("Java для чайников", "Чак Сфер", 2006);
+   
+        //  bookLib[0].getBook();
     }
 }
