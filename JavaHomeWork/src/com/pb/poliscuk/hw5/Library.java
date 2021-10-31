fdc08c87db5e2c20969c4dc99f0e65977b5fb98e
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-31 21:57:21 +0200

    init commit

--- JavaHomeWork/src/com/pb/poliscuk/hw5/Library.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw5/Library.java
@@ -7,9 +7,16 @@ public class Library {
    static  Book[] bookLib = new Book[5];
 
     public static void main(String[] args) {
-          
+        
+        for (int i=0; i<5; i++) bookLib[i] = new Book();
           bookLib[0].setBook("Java для чайников", "Чак Сфер", 2006);
+          bookLib[1].setBook("Книга Чистий код. Створення і рефакторинг за допомогою Agile", "Роберт Сесил Мартин", 2019);
+          bookLib[2].setBook("Основы Java", "Николай Прохоренок", 2019);
+          bookLib[3].setBook("Як пасти котів", "Дж. Ханк Рейнвотер", 2020);
+          bookLib[4].setBook("Стів Джобс", "Уолтер Айзексон", 2011);
+          
+          
            
-          bookLib[0].getBook();
+          for (int i=0; i<5; i++) bookLib[i].getBook();
     }
 }
