2a302622a0ba3a477805c7c46441a127db5027d3
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-11-05 21:41:03 +0200

    init commit

--- JavaHomeWork/src/com/pb/poliscuk/hw6/VetСlinic.java
+++ JavaHomeWork/src/com/pb/poliscuk/hw6/VetСlinic.java
@@ -1,24 +1,32 @@
 package com.pb.poliscuk.hw6;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
 public class VetСlinic {
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-       
-         
-        Animal[] myAnimal = new Animal[3] ;
-        myAnimal[0]= new Dog();
+
+        Animal[] myAnimal = new Animal[3];
+         Class clazz = myAnimal.getClass();
+        myAnimal[0] = new Dog();
         myAnimal[1] = new Cat();
         myAnimal[2] = new Horse();
-        
-        
-     Class veterinarian = Class.forName("Veterinarian");
-        Object object = veterinarian.newInstance();
-        
-        for (int i=0; i<myAnimal.length; i++)
-        {
-           
-        }
-        
-    }
-    }
 
+        System.out.println(clazz.getName());
+        System.out.println(Arrays.toString(clazz.getMethods()));
+        
+        
+      //  Class veterinarian = Class.forName("Veterinarian");
+      //  Object object = veterinarian.newInstance();
+        Class myClinic = Class.forName("Veterinarian");
+        
+
+      
+      
+
+        for (int i = 0; i < myAnimal.length; i++) {
+         //   ((Veterinarian)object).treatAnimal(myAnimal[i]);
+        }
+    }
+}
